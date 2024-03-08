63c0aa5ac8d96d56fe0dcfbb90e632c0962b5314
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2024-03-08 17:16:20 +0000

    CSS Files Styling globally instead of individually

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 
 import Header from "./components/ui/header/header.js";
+import Footer from "./components/ui/footer/footer.js";
 
 function App() {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 }
---
+++ src/components/ui/footer/footer.js
@@ -0,0 +1,11 @@
+import "./footer.css";
+
+function Footer() {
+  return (
+    <footer>
+      <h3>Hello World!</h3>
+    </footer>
+  );
+}
+
+export default Footer;
