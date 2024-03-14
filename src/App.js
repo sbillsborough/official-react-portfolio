0624fb5356d2acbf93edfdcedfbcb09383a6dc71
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2024-03-14 13:03:35 +0000

    Add Routing and Home Page
    installed react router
    Added routes to the App.js
    Added navlink to the home page
    Wrapped App component inside of Hashrouter in index.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/ui/home/home.js";
 
 import Header from "./components/ui/header/header.js";
-import Footer from "./components/ui/footer/footer.js";
 
 function App() {
   return (
     <>
       <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   );
 }
--- src/components/ui/header/header.js
+++ src/components/ui/header/header.js
@@ -1,7 +1,15 @@
+import { NavLink } from "react-router-dom";
+
 function Header() {
   return (
     <header>
       <h3>Hello World!</h3>
+
+      <div className="navbar">
+        <nav>
+          <NavLink to="/">Home</NavLink>
+        </nav>
+      </div>
     </header>
   );
 }
